9c2b55504419cd8ac6647b5d9702f29db98a7ef2
Hoomaan <id@hoomaan.dev>
2024-03-08 15:50:22 +0400

    Admin - Add Settings API Section

--- admin/settings-register.php
+++ admin/settings-register.php
@@ -28,6 +28,33 @@ function wp_gamefetcher_register_settings() {
 		'wp_gamefetcher_options', 
 		'wp_gamefetcher_options', 
 		'wp_gamefetcher_callback_validate_options' 
-	); 
+	);
+
+    /*
+	
+	add_settings_section( 
+		string   $id, 
+		string   $title, 
+		callable $callback, 
+		string   $page
+	);
+	
+	*/
+	
+	add_settings_section( 
+		'wp_gamefetcher_section_api', 
+		esc_html__('API Settings', 'wp-gamefetcher'), 
+		'wp_gamefetcher_callback_section_api', 
+		'wp_gamefetcher'
+	);
 }
 add_action( 'admin_init', 'wp_gamefetcher_register_settings' );
+
+
+
+// callback: API section
+function wp_gamefetcher_callback_section_api() {
+	
+	echo '<p>'. esc_html__('These settings enable you to customize the WP Login screen.', 'wp-gamefetcher') .'</p>';
+	
+}
